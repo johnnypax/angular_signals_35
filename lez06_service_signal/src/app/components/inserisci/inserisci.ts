ead5bb2206bea227f6904b820e25685a573c2977
Giovanni Pace <g.pace@braintechinnovations.it>
2026-09-25 10:22:23 +0200

    Concluso Service

--- lez06_service_signal/src/app/components/inserisci/inserisci.ts
+++ lez06_service_signal/src/app/components/inserisci/inserisci.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TodoService } from '../../services/todo-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   selector: 'app-inserisci',
   styleUrl: './inserisci.css',
   templateUrl: './inserisci.html',
 })
-export class Inserisci {}
+export class Inserisci {
+
+  service = inject(TodoService)
+  input_titolo?: string
+
+  aggiungi(){
+    if(this.input_titolo){
+      this.service.addTodo(this.input_titolo)
+      alert("STAPPOOOOOOOO")
+      return;
+    }
+
+    alert("ERRORE")
+  }
+
+}
